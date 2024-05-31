e776afd8979221065ca5a7c5021be121a85e9a06
Brezzor <71772224+Brezzor@users.noreply.github.com>
2024-05-31 11:30:29 +0200

    Update LlmChatClient.cs

--- LlmChat/LlmChatClient.cs
+++ LlmChat/LlmChatClient.cs
@@ -1,36 +1,60 @@
-﻿using CCRM2.Shared.Models;
+using CCRM2.Shared.Models;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
-using System;
+using Azure.AI.OpenAI;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CCRM2.Client.Services
 {
     public class LlmChatClient
     {
+        private readonly AppData _appData;
         private readonly HttpClient _httpClient;
-        public ObservableCollection<AiMessage> Messages { get; set; }
+        private ObservableCollection<AiMessage> Messages { get; set; }
 
-        public LlmChatClient(HttpClient httpClient)
+        public LlmChatClient(AppData appData, HttpClient httpClient)
         {
+
+            _appData = appData;
             _httpClient = httpClient;
             Messages = new ObservableCollection<AiMessage>();
         }
 
         public async Task<AiMessage> SendMessageAsync(string userInput)
         {
-            if (string.IsNullOrEmpty(userInput))
+            Messages.Add(new AiMessage()
             {
-                throw new ArgumentException("User input cannot be null or empty", nameof(userInput));
-            }
+                Role = ChatRole.User.ToString(),
+                Content = new Content()
+                {
+                    Message = userInput
+                }
+            });
 
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/llmchat2", userInput);
-            response.EnsureSuccessStatusCode(); // Throws if the response status is not successful
+            LlmRequest request = new LlmRequest()
+            {
+                CurrentEmployee = _appData.CurrentEmployee.ID,
+                Message = userInput,
+                AiMessages = Messages.ToList(),
+            };
+
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/llmchat", request);
 
             AiMessage message = await response.Content.ReadFromJsonAsync<AiMessage>();
+            
+            Messages.Add(message);
+
             return message;
         }
+
+        public ObservableCollection<AiMessage> GetMessages()
+        {
+            return new ObservableCollection<AiMessage>(Messages);
+        }
+
         public void ClearChat()
         {
             Messages.Clear();
