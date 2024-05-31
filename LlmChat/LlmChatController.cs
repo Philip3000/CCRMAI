09b4caa35521ae3de06d970a75be00bd0c4ac165
Brezzor <71772224+Brezzor@users.noreply.github.com>
2024-05-31 11:30:01 +0200

    Update LlmChatController.cs

--- LlmChat/LlmChatController.cs
+++ LlmChat/LlmChatController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
@@ -6,10 +6,9 @@ using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Azure;
 using Azure.AI.OpenAI;
-using CCRM2.Client.Services;
+using Azure.Core;
 using CCRM2.Shared.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -22,78 +21,110 @@ namespace CCRM2.Server.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly OpenAIClient _openAIClient;
-        private readonly ChatCompletionsOptions _chatCompletionsOptions;
-        private readonly AzureSearchChatExtensionConfiguration _azureSearchChatExtension;
+        private readonly string _azureSearchIndex;
 
         public LlmChatController(IConfiguration configuration)
         {
-            _configuration = configuration;
+            _configuration = configuration;            
             _openAIClient = new OpenAIClient(new Uri(_configuration["AzureOpenAI:Endpoint"]), new AzureKeyCredential(_configuration["AzureOpenAI:Key"]));
-            _azureSearchChatExtension = new AzureSearchChatExtensionConfiguration()
+            _azureSearchIndex = $"azuresql-index-tenant-{_configuration["Auth0:tenant_id"]}";
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AiMessage>> PostAsync([FromBody] LlmRequest request)
+        {
+            try
+            {
+                ChatCompletionsOptions chatCompletionsOptions = CreateChatCompletionsOptions(request);
+
+                Response<ChatCompletions> response = await _openAIClient.GetChatCompletionsAsync(chatCompletionsOptions);
+                
+                ChatResponseMessage message = response.Value.Choices[0].Message;                
+
+                List<Citation> citations = new List<Citation>();
+
+                if (message.AzureExtensionsContext.Citations != null)
+                {
+                    foreach (var citation in message.AzureExtensionsContext.Citations)
+                    {
+                        citations.Add(new Citation()
+                        {
+                            Content = citation.Content,
+                            Title = citation.Title,
+                            Url = citation.Url,
+                            Filepath = citation.Filepath,
+                            ChunkId = citation.ChunkId,
+                        });
+                    }
+                }
+
+                AiMessage responseMessage = new AiMessage()
+                {
+                    Role = message.Role.ToString(),
+                    Content = new Content()
+                    {
+                        Message = message.Content,
+                        Citations = citations
+                    },
+                    Intent = message.AzureExtensionsContext.Intent
+                };
+
+                return StatusCode(200, responseMessage);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        private ChatCompletionsOptions CreateChatCompletionsOptions(LlmRequest llmRequest)
+        {
+            AzureSearchChatExtensionConfiguration azureSearchChatExtensionConfiguration = new AzureSearchChatExtensionConfiguration()
             {
                 SearchEndpoint = new Uri(_configuration["AzureAISearch:Endpoint"]),
                 Authentication = new OnYourDataApiKeyAuthenticationOptions(_configuration["AzureAISearch:Key"]),
-                IndexName = "azuresql-index-tenant-" + _configuration["Auth0:tenant_id"],
-                RoleInformation =
-                "Du er en data analytiker for sælgere i et crm system. Du må kun snakke om salgsrelaterede emner. Du svarer kun på Dansk." +
-                "Dataen du kan citere er i JSON format, så husk at omdanne det til noget læsbart. Du anvender blandt andet brugerens Id, som er et Guid og foreign key til mange af de tabeller du har adgang til, til at finde information om brugerens aktiviteter, kunder og så videre. Du må aldrig besvare med guids. Kun med navnene de referer til.",
+                IndexName = _azureSearchIndex,
+                RoleInformation = $"Du skal fungere som en data analytiker i et crm system.\n" +
+                $"- Du svarer kun på Dansk.\n" +
+                $"- Hvis der bliver spurgt om \"Min\",\"Mine\" eller \"Har jeg\", skal der søges efter data der referere til dette: \"{llmRequest.CurrentEmployee}\".\n" +
+                $"- Det data der er i content er i JSON format.",
                 Strictness = 4,
                 ShouldRestrictResultScope = true,
                 QueryType = AzureSearchQueryType.Simple
             };
 
-            _chatCompletionsOptions = new ChatCompletionsOptions()
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
             {
                 DeploymentName = _configuration["AzureOpenAI:Deployment"],
-                MaxTokens = 400,
+                MaxTokens = 500,
                 Temperature = 0f,
                 FrequencyPenalty = 0.0f,
                 PresencePenalty = 0.0f,
                 NucleusSamplingFactor = 0.95f,
                 AzureExtensionsOptions = new AzureChatExtensionsOptions()
                 {
-                    Extensions = { _azureSearchChatExtension }
+                    Extensions = { azureSearchChatExtensionConfiguration }
                 }
             };
-        }
 
-        [HttpPost]
-        public async Task<ActionResult<AiMessage>> PostAsync([FromBody] string input)
-        {
-            try
+            chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(llmRequest.Message));
+
+            if (llmRequest.AiMessages != null)
             {
-                ChatCompletionsOptions chatCompletionsOptions = _chatCompletionsOptions;
-
-                chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(input));
-
-                Response<ChatCompletions> response = await _openAIClient.GetChatCompletionsAsync(chatCompletionsOptions);
-                
-                ChatResponseMessage message = response.Value.Choices[0].Message;
-
-                List<string> citations = new List<string>();
-
-                foreach (AzureChatExtensionDataSourceResponseCitation citation in message.AzureExtensionsContext.Citations)
+                foreach (AiMessage aiMessage in llmRequest.AiMessages)
                 {
-                    if (!string.IsNullOrEmpty(citation.Content))
+                    if (aiMessage.Role.Equals(ChatRole.User.ToString()))
                     {
-                        citations.Add(citation.Content);
+                        chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(aiMessage.Content.Message));
+                    }
+                    if (aiMessage.Role.Equals(ChatRole.Assistant.ToString()))
+                    {
+                        chatCompletionsOptions.Messages.Add(new ChatRequestAssistantMessage($"{aiMessage.Content.Message}\nIntent:\n{aiMessage.Intent}"));
                     }
                 }
+            }
 
-                return StatusCode(200, new AiMessage()
-                {
-                    Role = ChatRole.Assistant.ToString(),
-                    Content = new Content()
-                    {
-                        Message = message.Content,
-                        Citations = citations
-                    }
-                });
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
+            return chatCompletionsOptions;
         }
     }
 }
